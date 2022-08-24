ad467b61de3b8f5da173a8bf1858818d02067ca7
Carlos Souza <carlos.pessoal@hotmail.com>
2022-08-24 14:36:12 -0300

    feat(auth): validate cpf on createUserValidator

--- apps/auth/src/infra/http/middlewares/createUserValidator.ts
+++ apps/auth/src/infra/http/middlewares/createUserValidator.ts
@@ -1,11 +1,15 @@
 import z from 'zod'
 import { Request } from 'express'
+import { validateCPF } from 'validations-br'
 
 import { ok, unprocessableEntity, expressMiddlewareAdapter } from '@cube/common'
 
 const createUserSchema = z.object({
-  cpf: z.string().length(11).regex(/^\d{11}$/g),
-  name: z.string(),
+  cpf: z.string()
+    .length(11)
+    .regex(/^\d{11}$/g)
+    .refine((val) => validateCPF(val), { message: 'Field must be a valid cpf' }),
+  name: z.string().refine((val) => val.split(' ').length >= 2, { message: 'Field must have fullname value' }),
   email: z.string().email(),
   password: z.string().length(4).regex(/^\d{4}$/g)
 })
