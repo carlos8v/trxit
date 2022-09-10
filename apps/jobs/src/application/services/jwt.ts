5b0f7a5f6b887b58d17a4dc50b6068aa2922961c
Carlos Souza <carlos.pessoal@hotmail.com>
2022-09-10 11:56:00 -0300

    feat(jobs): set issuer on jwt validation

--- apps/jobs/src/application/services/jwt.ts
+++ apps/jobs/src/application/services/jwt.ts
@@ -1,16 +1,16 @@
-import jwt, { SignOptions } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { jwtExpireTime } from '@cube/common'
 
 const JWT_SECRET = process.env.JWT_SECRET!
 
-const jwtOptions: SignOptions = {
-  expiresIn: jwtExpireTime.accessToken.formated
-}
+const jwtTokenExpires = jwtExpireTime.accessToken.formated
 
 const sign = (payload: any) => jwt.sign(
   payload,
-  JWT_SECRET, {
-    ...jwtOptions,
+  JWT_SECRET,
+  {
+    expiresIn: jwtTokenExpires,
+    issuer: '@cube/jobs',
     audience: '@cube/jobs'
   }
 )
@@ -20,6 +20,7 @@ const verify = (accessToken: string) => jwt.verify(
   JWT_SECRET,
   {
     algorithms: ['HS256'],
+    issuer: '@cube/jobs',
     audience: '@cube/jobs'
   }
 )
