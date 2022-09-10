2516bf7ce8725e971b71d7e7ecf21d4ce4747d13
Carlos Souza <carlos.pessoal@hotmail.com>
2022-09-10 11:54:18 -0300

    feat(common): add audience list param on requireAuth for jwt validation

--- packages/common/src/middlewares/requireAuth.ts
+++ packages/common/src/middlewares/requireAuth.ts
@@ -17,17 +17,25 @@ declare global {
   }
 }
 
-export default expressMiddlewareAdapter((req) => {  
+export default (audience: string | string[]) => expressMiddlewareAdapter((req) => {  
   try {
     const authToken = req.headers['authorization']
     if (!authToken) throw new Error('Credenciais faltando')
 
     const [_, token] = authToken.split(' ')
-    const clientPayload = jwt.verify(token, process.env.JWT_SECRET)
+
+    const publicKey = Buffer.from(process.env.JWT_PUBLIC_KEY, 'base64')
+    const clientPayload = jwt.verify(token, publicKey, {
+      algorithms: ['RS256'],
+      issuer: '@cube/auth',
+      audience: audience
+    })
+
     req.currentClient = clientPayload as ClientPayload
 
     return ok()
   } catch(err) {
+    console.log(err)
     return unauthorized('Usuário não autenticado')
   }
 })
